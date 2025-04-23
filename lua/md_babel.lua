2eddafd0acb00d0c05d12c53e2ff0797ac45175e
Christian Tietze <me@christiantietze.de>
2025-04-23 08:14:08 +0200

    test setup() affecting vim.g

--- lua/md_babel.lua
+++ lua/md_babel.lua
@@ -10,23 +10,25 @@ local config = {
 
 local M = {}
 
-M.config = config
+---@param opts Config?
+M.setup = function(opts)
+  opts = opts or config
 
----@param args Config?
-M.setup = function(args)
-  M.config = vim.tbl_deep_extend("force", M.config, args or {})
+  if not opts.md_babel_path then
+    error "'md_babel_path' value required"
+  end
 
   -- TODO: Is this pattern normal? Move vim.g.md_babel_executable_path solely into local config?  https://github.com/md-babel/md-babel.nvim/issues/1
-  vim.g.md_babel_executable_path = M.config.md_babel_path
+  vim.g.md_babel_executable_path = opts.md_babel_path
 
-  vim.api.nvim_create_user_command('MdBabelExec', function()
-                                     module.execute_block_at_point()
-  end, {})
+  vim.api.nvim_create_user_command('MdBabelExec', module.execute_block_at_point, {})
 
-  if M.config.keymap then
-    vim.keymap.set('n', M.config.keymap, M.execute_block_at_point,
+  if opts.keymap then
+    vim.keymap.set('n', opts.keymap, module.execute_block_at_point,
                    {noremap = true, silent = true, desc = "Execute md-babel with code block at cursor"})
   end
 end
+
+M.execute_block_at_point = module.execute_block_at_point
 
 return M
